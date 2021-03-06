8e67643de4e5746314f8c59be503156253654c75
jbh3474 <joseph_b_harrell@homedepot.com>
2021-03-06 12:18:51 -0500

    adding testnet get files functions

--- get_latest_config_files.sh
+++ get_latest_config_files.sh
@@ -7,12 +7,15 @@ BASELINK="https://hydra.iohk.io/job/Cardano/iohk-nix/cardano-deployment/latest-f
 #curl -sSL ${BASELINK}testnet-byron-genesis.json -o config/testnet-byron-genesis.json
 #curl -sSL ${BASELINK}testnet-topology.json -o config/topology.json
 
-# Mainnet Candidate
-#echo "Downloading mainnet_candidate files..."
-#curl -sSL ${BASELINK}mainnet_candidate_4-config.json -o config/mainnet_candidate-config.json
-#curl -sSL ${BASELINK}mainnet_candidate_4-shelley-genesis.json -o config/mainnet_candidate-shelley-genesis.json
-#curl -sSL ${BASELINK}mainnet_candidate_4-byron-genesis.json -o config/mainnet_candidate-byron-genesis.json
-#curl -sSL ${BASELINK}mainnet_candidate_4-topology.json -o config/mainnet_candidate-topology.json
+## Testnet
+#wget https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/testnet-config.json -O ../config.json
+#wget https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/testnet-byron-genesis.json -O ../testnet-byron-genesis.json
+#wget https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/testnet-shelley-genesis.json -O ../testnet-shelley-genesis.json
+#wget https://hydra.iohk.io/job/Cardano/cardano-node/cardano-deployment/latest-finished/download/1/testnet-topology.json -O ../topology.json
+
+
+
+
 
 # # Mainnet
 echo "Downloading mainnet files..."
