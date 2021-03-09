d4b1e17d34c153e9fe716e504991e23ded71db14
jbh3474 <joseph_b_harrell@homedepot.com>
2021-03-09 01:02:55 -0500

    adding steps on how to install executable

--- sever-setup.sh
+++ sever-setup.sh
@@ -88,6 +88,8 @@ ufw allow 2222/tcp  # ssh port
 ufw allow 3001/tcp  # cardano-node port
 ufw allow 9100/tcp  # prometheus port
 ufw allow 12798/tcp # prometheus port
+ufw allow 8080/http # RTView port
+ufw allow 8181/http # RTView port
 ufw enable
 
 # Reboot
